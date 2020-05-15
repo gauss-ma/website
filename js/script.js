2aeb8647fdc7d08069c6896ffbe2c899b2b500e5
ramespada <espada@agro.uba.ar>
2020-05-15 09:09:55 -0300

    init

---
+++ js/script.js
@@ -0,0 +1,218 @@
+// Here u can do your maggic ;)
+//
+//
+const $menu=$('.TabletMenu_root');
+const $btn_menu=$('#botonMenu');
+const $logo=$(".Logo_logo");
+
+$(document).ready(function(){
+
+
+        cerrarMenu();
+
+        //ABRIR / CERRAR MENU:
+        function abrirMenu(){
+                console.log("Abriendo Menu..");
+                $menu.addClass("activo");
+                $btn_menu.addClass('burguer_activo');
+                $btn_menu.removeClass('burger_inactivo');
+                $menu.show();
+                $("body").addClass("touch-off no-scroll");
+                setBlack();
+        };
+
+        function cerrarMenu(){
+                console.log("Cerrando Menu..");
+                $menu.removeClass("activo");
+                $btn_menu.addClass('burger_inactivo');
+                $btn_menu.removeClass('burguer_activo');
+                $menu.slideUp(); // $menu.hide();
+                $("body").removeClass("touch-off no-scroll");
+                //setWhite();
+        };
+
+        $btn_menu.on('click',function () {
+                if ( $menu.attr('class') == "TabletMenu_root activo") {cerrarMenu()}
+                else if ( $menu.attr('class') == "TabletMenu_root") {abrirMenu()}
+                else {consola.log("tamo todos locos")};
+        });
+
+
+        // FUNCION PARA SCROLLEAR CUANDO APRETAS UN ITEM DEL MENU:
+        $('nav a').on('click', function() {
+            var scrollAnchor = $(this).attr('data-scroll'),
+                scrollPoint = $('div[name="' + scrollAnchor + '"]').offset().top - 0;
+
+            $('body,html').animate({
+                scrollTop: scrollPoint
+            }, 500);
+
+            cerrarMenu()
+
+            return false;
+        })
+
+
+        $(window).scroll(function() {
+            var windscroll = $(window).scrollTop();
+            if (windscroll >= 100) {
+                $('nav').addClass('fixed');
+                $('.wrapper section').each(function(i) {
+                    if ($(this).position().top <= windscroll - 20) {
+                        $('nav a.active').removeClass('active');
+                        $('nav a').eq(i).addClass('active');
+                    }
+                });
+
+            } else {
+
+                $('nav').removeClass('fixed');
+                $('nav a.active').removeClass('active');
+                $('nav a:first').addClass('active');
+            }
+
+        }).scroll();
+
+        //SCROLLEAR HASTA ARRIBA CUANDO CLICKEAS EN EL LOGO
+        $logo.on('click', function() {
+            $('body,html').animate({
+                scrollTop: 0
+                }, 500);
+
+            cerrarMenu();
+            return false;
+        })
+        //SCROLLEAR HASTA lista de servicios cuando clickias un icono de servicios
+        $('.icon').on('click', function() {
+            var scrollAnchor = $(this).attr('data-scroll'),
+            scrollPoint = $('.icon_wrapper').offset().top - 30;
+            $('body,html').animate({ scrollTop: scrollPoint }, 500);
+
+            return false;
+        })
+
+	//Acciones que ocurren a medida que scrolleo:
+
+        // CAMBIAR COLOR DE LOGO Y MENU-ICON SEGUN EL BACKGROUND/SECCION
+        function setBlack(){
+                $logo.removeClass("white");
+                $logo.addClass("black");
+                $btn_menu.removeClass("white");
+                $btn_menu.addClass("black");
+        };
+
+        function setWhite(){
+                $logo.removeClass("black");
+                $logo.addClass("white");
+                $btn_menu.removeClass("black");
+                $btn_menu.addClass("white");
+        };
+
+        //
+        function showObjetivoText(){
+                // renred-word word_root word_show-now
+                $( ".renred-word" ).each(function( index ) {
+                        $(this).addClass("word_show-now");
+                });
+
+        ;}
+        
+ 	pos_home = $("div[name=home]").position().top;             
+ 	pos_objetivo = $("div[name=objetivo]").position().top;
+ 	pos_modelos = $("div[name=modelos]").position().top;
+ 	pos_productos = $("div[name=productos]").position().top;
+ 	pos_servicios = $("div[name=servicios]").position().top;       
+ 	pos_contacto = $("div[name=contacto]").position().top;
+ 	pos_office = $("div[name=office]").position().top;
+
+	$(window).on("scroll", function() {
+            var position = $(window).scrollTop()
+            console.log(position);
+                 if (position > pos_objetivo  & position < pos_modelos  ) { setWhite(); showObjetivoText();  console.log("Objetivo!!"); }
+            else if (position > pos_modelos   & position < pos_productos) { setBlack();$(".Modelos").addClass("faded")  ;  console.log("Modelos!!");  }
+            else if (position > pos_productos & position < pos_servicios) { setBlack();$(".Productos").addClass("faded");  console.log("Productos!!");}
+            else if (position > pos_servicios & position < pos_contacto ) { setBlack();$(".Servicios").addClass("faded");  console.log("Servicios!!");}
+            else if (position > pos_contacto  & position < pos_office   ) { setBlack();$(".Contacto").addClass("faded") ;  console.log("Contacto!!"); }
+            else if (position > pos_office ){ setWhite();}
+            else{setBlack();}
+        });
+
+});
+
+
+//Para el slider de SERVICIOS
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// Formulario de contacto--
+
+// var $form = $('form#test-form'),
+//     url = 'https://script.google.com/macros/s/AKfycbxYwrCZzUoD8FhYrQ1KUGdDpicMMCi8CwLwkJAmTqlQ-v41C8Bd/exec'
+//
+//$('#submit-form').on('click', function(e) {
+//    console.log("click")
+//    console.log(document.getElementsByTagName("input")[10].value) //manera alternativa de levantar los inputs, los 10 inputs anteriores a estos son los checkbox.
+//    console.log(document.getElementsByTagName("input")[11].value)
+//    console.log(document.getElementsByTagName("input")[12].value)
+//    
+//    var x = new XMLHttpRequest();
+//    x.open("GET","phpmailer/envio.php",true);
+//    x.send();
+//    console.log('Enviado');
+//    return false;
+//
+//})
+//
+
+//Cosas que pruebo y no funcionan
+
+
+//     var xhr = new XMLHttpRequest();
+//     var url = "https://script.google.com/macros/s/AKfycbxYwrCZzUoD8FhYrQ1KUGdDpicMMCi8CwLwkJAmTqlQ-v41C8Bd/exec" + encodeURIComponent(JSON.stringify({"email": email}));
+//     xhr.open("GET", url, true);
+//     xhr.setRequestHeader("Content-Type", "application/json");
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState === 4 && xhr.status === 200) {
+//             var json = JSON.parse(xhr.responseText);
+//             console.log(json.email);
+//         }
+//     };
+//     xhr.send();
+//     console.log(json)
+
+//     //e.preventDefault();
+// //   var jqxhr = $.ajax({
+// //      url: 'https://script.google.com/macros/s/AKfycbxYwrCZzUoD8FhYrQ1KUGdDpicMMCi8CwLwkJAmTqlQ-v41C8Bd/exec',
+// //      method: "GET",
+// //     dataType: "json",
+// //     data: $('form#test-form').serializeObject()
+
+//   }).success(
+//     console.log("mensaje enviado") // do something
+//   )
+//   //})
---
+++ php/mail.php
@@ -0,0 +1,67 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+$mensaje_a_gauss="De: $nombre <br> Email: $email <br> Mensaje:<br> $mensaje";
+$mensaje_a_usuario="Tu mensje ha sido enviado exitosamente!<br> A la brevedad algún representante de GAUSS se estará comunicando con vos.<br> ------------------------------<br> Copia del mensaje enviado:<br> $mensaje_a_gauss <br><br>------------------------------<br>"; 
+
+$mail = new PHPMailer(true);
+
+try {
+    //$mail->SMTPDebug = 2;  				// Sacar esta línea para no mostrar salida debug
+    $mail->isSMTP();
+    $mail->Host = 'mail.gauss-ma.com.ar';               // Host de conexión SMTP
+    $mail->SMTPAuth = true;
+    $mail->Username = 'mailer@gauss-ma.com.ar';         // Usuario SMTP
+    $mail->Password = '^!DCFM3%(B!q';                   // Password SMTP
+    $mail->SMTPSecure = 'tls';                          // Activar seguridad TLS
+    $mail->Port = 587;                                  // Puerto SMTP
+
+    $mail->setFrom('mailer@gauss-ma.com.ar');		// Mail del remitente
+    $mail->addAddress("espada@gauss-ma.com.ar");        // Mail del destinatario
+    $mail->addAddress("medrano@gauss-ma.com.ar");       // Mail del destinatario
+ 
+    $mail->isHTML(true);
+    $mail->Subject = "Formulario desde GAUSS de $nombre."; // Asunto del mensaje
+    $mail->Body    ="$mensaje_a_gauss";                    //Este es el contenido del mensaje <b>en negrita!</b>';    // Contenido del mensaje (acepta HTML)
+    $mail->AltBody ="$mensaje_a_gauss";                    // 'Este es el contenido del mensaje en texto plano';    // Contenido del mensaje alternativo (texto plano)
+ 
+    $mail->send();
+    echo "<script>alert('El mensaje ha sido enviado exitosamente!');</script>";
+} catch (Exception $e) {
+    echo "<script>alert('El mensaje no se ha podido enviar, ERROR:</script>",$mail->ErrorInfo;
+}
+
+$mail_a_usr= new PHPMailer(true);
+
+try {
+    $mail_a_usr->isSMTP();
+    $mail_a_usr->Host = 'mail.gauss-ma.com.ar';                 // Host de conexión SMTP
+    $mail_a_usr->SMTPAuth = true;
+    $mail_a_usr->Username = 'mailer@gauss-ma.com.ar';           // Usuario SMTP
+    $mail_a_usr->Password = '^!DCFM3%(B!q';                     // Password SMTP
+    $mail_a_usr->SMTPSecure = 'tls';                            // Activar seguridad TLS
+    $mail_a_usr->Port = 587;                                    // Puerto SMTP
+
+    $mail_a_usr->setFrom('mailer@gauss-ma.com.ar');		// Mail del remitente
+    $mail_a_usr->addAddress("$email");                          // Mail del destinatario
+ 
+    $mail_a_usr->isHTML(true);
+    $mail_a_usr->Subject = "[GAUSS] (no responder) Tu mensaje ha sido enviado exitosamente!";  // Asunto del mensaje
+    $mail_a_usr->Body    ="$mensaje_a_usuario";   //Este es el contenido del mensaje <b>en negrita!</b>';    // Contenido del mensaje (acepta HTML)
+    $mail_a_usr->AltBody ="$mensaje_a_usuario";   // 'Este es el contenido del mensaje en texto plano';    // Contenido del mensaje alternativo (texto plano)
+    $mail_a_usr->send();
+} catch (Exception $e) {
+}
+
+?>
