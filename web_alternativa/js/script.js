e2cca51341072f9f60a7790b6bcc298869ffb4c7
ramespada <espada@agro.uba.ar>
2020-07-17 15:15:42 -0300

    primer planteo de parte de servicios

--- web_alternativa/js/script.js
+++ web_alternativa/js/script.js
@@ -141,7 +141,48 @@ $(document).ready(function(){
         //   //else if (position > pos_office ){ setWhite();}
         //   else{setBlack();}
         //});
-
+		 curOpen = $('.step')[0];
+		  
+		  $('.next-btn').on('click', function() {
+		    let cur = $(this).closest('.step');
+		    let next = $(cur).next();
+		    $(cur).addClass('minimized');
+		    setTimeout(function() {
+		      $(next).removeClass('minimized');
+		      curOpen = $(next);
+		    }, 400);
+		  });
+		  
+		  $('.close-btn').on('click', function() {
+		    let cur = $(this).closest('.step');
+		    $(cur).addClass('minimized');
+		    curOpen = null;
+		  });
+		  
+		  $('.step .step-content').on('click' ,function(e) {
+		    e.stopPropagation();
+		  });
+		  
+		  $('.step').on('click', function() {
+		    if (!$(this).hasClass("minimized")) {
+		      curOpen = null;
+		      $(this).addClass('minimized');
+		    }
+		    else {
+		      let next = $(this);
+		      if (curOpen === null) {
+		        curOpen = next;
+		        $(curOpen).removeClass('minimized');
+		      }
+		      else {
+		        $(curOpen).addClass('minimized');
+		        setTimeout(function() {
+		          $(next).removeClass('minimized');
+		          curOpen = $(next);
+		        }, 300);
+		      }
+		    }
+		  });
 });
 
 
